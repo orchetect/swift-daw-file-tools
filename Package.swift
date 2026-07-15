2244311994ee0c44b96e0cb55724edb40002958b
Steffan Andrews <stef@orchetect.com>
2026-07-14 19:43:03 -0700

    Bumped SwiftFCPXML dependency to 0.1.2

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/orchetect/swift-extensions", from: "3.0.0"),
-        .package(url: "https://github.com/orchetect/swift-fcpxml", from: "0.1.1"),
+        .package(url: "https://github.com/orchetect/swift-fcpxml", from: "0.1.2"),
         .package(url: "https://github.com/orchetect/swift-timecode", from: "3.1.1"),
         .package(url: "https://github.com/orchetect/swift-midi-file", from: "1.0.2"),
 
